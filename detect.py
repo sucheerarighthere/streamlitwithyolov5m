98e2123bbf58396eabc05c9cdeaf631bf944ede5
coffiholic <108257436+sucheerarighthere@users.noreply.github.com>
2024-03-11 13:56:55 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -49,7 +49,7 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 @smart_inference_mode()
 def run(
-        weights='models/bestyolo.pt',  # model path or triton URL
+        weights=ROOT /'yolov5s.pt',  # model path or triton URL
         source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
         data='data.yaml' ,  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
